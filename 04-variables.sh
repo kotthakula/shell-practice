9ded6baf3ccccf9fde111f28d1f1b192311d1be7
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 15:00:51 +0530

    I have added variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,7 +2,7 @@
 PERSION1=$1
 PERSION2=$2
 
-echo "PERSION1:: Hi PERSION2, How are you?"
-echo "PERSION2:: I am good, How are you doing?"
-echo "PERSION1:: I am doing good, what's going on?"
-echo "PERSION2:: I have started to learn shell scripting"
+echo "$PERSION1:: Hi $PERSION2, How are you?"
+echo "$PERSION2:: I am good, How are you doing?"
+echo "$PERSION1:: I am doing good, what's going on?"
+echo "$PERSION2:: I have started to learn shell scripting"
