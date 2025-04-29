5cfe91a65e96ad854751a3366bd33afcfc4c53ef
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 14:53:04 +0530

    03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSION1=Priya
 PERSION2=Prerna
-echo "PERSION1:: Hi {PERSION2}, How are you?"
-echo "{PERSION2}:: Hi PERSION1, I am good, How are you doing"
-echo "PERSION1:: I am doing good, What's gong on?"
-echo "{PERSION2}:: I have started shell scripting PERSION1"
+echo "$PERSION1:: Hi {$PERSION2}, How are you?"
+echo "{$PERSION2}:: Hi $PERSION1, I am good, How are you doing"
+echo "$PERSION1:: I am doing good, What's gong on?"
+echo "{$PERSION2}:: I have started shell scripting $PERSION1"
