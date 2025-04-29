620f2936d067c770349ba7a968a3903e230f9d98
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 06:25:45 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"Priya" :: "Hello Prerna, How are you"
+"prerna" :: "Hi Priya, I am good. How are you doing"
+"Priya" :: I am doing good Prerna, what's going on"
+"Prerna" :: I hsve started shell scripting"
