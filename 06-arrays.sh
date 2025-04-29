d651c88d95f158e175b7a0bff2ec4c466be0235e
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 15:35:27 +0530

    I am printing the fruits

---
+++ 06-arrays.sh
@@ -0,0 +1,7 @@
+#print the Apple, Kiwi, Orange
+
+#!/bin/bash
+Fruits =("Apple" "Kiwi" "Orange")
+echo "First fruit is :{Fruit[0]}"
+echo "Second fruit is :{Fruit[1]}"
+echo "Third fruit is :{Fruit[2]}
