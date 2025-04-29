ebe02d1e8b204aef22ce8160a6a07a960fe46666
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 06:57:28 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSION1=Priya
+PERSION2=Prerna
+"PERSION1:: Hi {PERSION2}, How are you?"
+"{PERSION2}:: Hi PERSION1, I am good, How are you doing"
+"PERSION1:: I am doing good, What's gong on?"
+"{PERSION2}:: I have started shell scripting PERSION1"
