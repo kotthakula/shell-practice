699669adbe3d1dc071e74bd99b5ca534d1ad9930
kotthakula <sushmakotthakula2020@gmail.com>
2025-04-29 15:28:35 +0530

    I am adding the confidential information

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,13 +1,10 @@
 #how to give confidential information at run time
 
 #!/bin/bash
-# echo "please enter username"
-# read -s USERNAME
-# echo "Username entered is :$USERNAME"
-# echo "please enter password"
-# read -s PASSWORD
-# echo "Password entered is :$PASSWORD"
-
 echo "please enter username"
-read -s Username
-#echo "Username entered is :$Username"
+read -s USERNAME
+echo "Username entered is :$USERNAME"
+echo "please enter password"
+read -s PASSWORD
+echo "Password entered is :$PASSWORD"
+
