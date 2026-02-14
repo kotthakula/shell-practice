d17c01a49422654a02fb5a6d078fd699ec542dfa
kotthakula <sushmakotthakula2020@gmai.com>
2026-02-14 18:51:10 +0530

    conditions

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]
 then
-   echo "Given Number: $NUMER is greater than 20"
+   echo "Given Number: $NUMBER is greater than 20"
 else
    echo "Given Number: $NUMBER is less than 20"
 fi
