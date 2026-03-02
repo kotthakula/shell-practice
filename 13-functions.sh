c27af744acd0486112e2b19038d2062489811648
kotthakula <sushmakotthakula2020@gmai.com>
2026-03-02 23:34:59 +0530

    functions.sh

---
+++ 13-functions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is..SUCCESS"
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root priveleges"
+    exit1
+fi
+dnf list installed git
+
+
+
+if [$? -ne 0 ]
+then
+    echo "git is not installed.. going to install"
+    dnf install git -y
+    VALIDATE $? "Insatlling Git"
+else
+    echo "Git is already installed, nothing to do"
+
+dnf list install mysql 
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed.. gong to install"
+    dnf install mysql -y
+     VALIDATE $? "Insatlling Mql"
+   
+else
+    echo "MYSqL is already installed, nothing to do"
+fi
+
