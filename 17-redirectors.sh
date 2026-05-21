2ef2d2663916c2b5f28be0aeb6805230ba44ad41
kotthakula <sushmakotthakula2020@gmai.com>
2026-05-22 03:41:32 +0530

    redirectors

---
+++ 17-redirectors.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #(echo "16-redirectors.sh" | cut -d "." -f1 it will give 16-redirectors )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) 
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "$R Please run this script with root priveleges $Ns"  &>>LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R FAILED $N" &>>LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is..$G SUCCESS $N" &>>LOG_FILE
+    fi
+}
+
+CHECK_ROOT
+
+#sh 16-loops.sh git mysql postfix nginx
+for package in $@ #$@ refers to the passed to it
+do 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed.. going to install"
+        dnf install $package -y
+        VALIDATE $? "Insatlling $package"
+else
+    echo "$package is already installed, nothing to do $N" &>>LOG_FILE
+    fi
+done
